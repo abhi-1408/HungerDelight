8b915ed2674e36e713fcbf7fdcafd7618c317d63
Abhishek Saklani <saklani1408@gmail.com>
2020-09-29 11:02:42 +0530

    deleted old structure

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app.models import Merchants, Stores, Items, Orders, OrderItemMapping
-
-# Register your models here.
-admin.site.register(Merchants)
-admin.site.register(Stores)
-admin.site.register(Items)
-admin.site.register(Orders)
-admin.site.register(OrderItemMapping)
