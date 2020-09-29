8078b41ec4817d81c8b96cabb71dbd57614828ef
Abhishek Saklani <saklani1408@gmail.com>
2020-09-29 16:25:40 +0530

    updated models

--- hungerDelight/app/admin.py
+++ hungerDelight/app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from app.models import Merchants, Stores, Items, Orders, OrderItemMapping
+from app.models import Merchant, Store, Item, Order
+
 
 # Register your models here.
-admin.site.register(Merchants)
-admin.site.register(Stores)
-admin.site.register(Items)
-admin.site.register(Orders)
-admin.site.register(OrderItemMapping)
+admin.site.register(Merchant)
+admin.site.register(Store)
+admin.site.register(Item)
+admin.site.register(Order)
