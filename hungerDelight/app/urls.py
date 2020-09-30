30259b9e329191ae37def3388f235819494163d5
Abhishek Saklani <saklani1408@gmail.com>
2020-09-30 10:09:39 +0530

    changed to viewset

--- hungerDelight/app/urls.py
+++ hungerDelight/app/urls.py
@@ -1,14 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
-from app import views
+from django.urls import path, include
+from .views import MerchantViewSet, ItemViewSet, StoreViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('merchant', MerchantViewSet)
+router.register('store', StoreViewSet)
+router.register('item', ItemViewSet)
 
 urlpatterns = [
-    path('', views.index),
-    path('merchant/', views.merchant_list_view),
-    path('merchant/<int:pk>/', views.merchant_detail_view),
-    path('store/', views.store_list_view),
-    path('store/<int:pk>/', views.store_detail_view),
-    path('item/', views.item_list_view),
-    path('item/<int:pk>/', views.item_detail_view),
+
+    path('', include(router.urls)),
+
 ]
--- hungerDelight/app/views.py
+++ hungerDelight/app/views.py
@@ -1,54 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .serializers import MerchantSerializer, StoreSerializer, ItemSerializer
 from .models import Merchant, Store, Item
+from rest_framework import viewsets
 # Create your views here.
 
 
-@api_view(['GET'])
-def index(request):
-    return Response('HI EVERYONE')
+class MerchantViewSet(viewsets.ModelViewSet):
+    serializer_class = MerchantSerializer
+    queryset = Merchant.objects.all()
 
 
-@api_view(['GET'])
-def merchant_list_view(request):
-    merchants = Merchant.objects.all()
-    merchant_serializer = MerchantSerializer(merchants, many=True)
-    return Response(merchant_serializer.data)
+class StoreViewSet(viewsets.ModelViewSet):
+    serializer_class = StoreSerializer
+    queryset = Store.objects.all()
 
 
-@api_view(['GET'])
-def merchant_detail_view(request, pk):
-    merchants = Merchant.objects.get(id=pk)
-    merchant_serializer = MerchantSerializer(merchants, many=False)
-    return Response(merchant_serializer.data)
-
-
-@api_view(['GET'])
-def store_list_view(request):
-    stores = Store.objects.all()
-    store_serializer = StoreSerializer(stores, many=True)
-    return Response(store_serializer.data)
-
-
-@api_view(['GET'])
-def store_detail_view(request, pk):
-    stores = Store.objects.get(id=pk)
-    store_serializer = StoreSerializer(stores, many=False)
-    return Response(store_serializer.data)
-
-
-@api_view(['GET'])
-def item_list_view(request):
-    items = Item.objects.all()
-    item_serializer = ItemSerializer(items, many=True)
-    return Response(item_serializer.data)
-
-
-@api_view(['GET'])
-def item_detail_view(request, pk):
-    items = Item.objects.get(id=pk)
-    item_serializer = ItemSerializer(items, many=False)
-    return Response(item_serializer.data)
+class ItemViewSet(viewsets.ModelViewSet):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
