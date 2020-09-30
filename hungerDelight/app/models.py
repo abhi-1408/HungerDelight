ec15d58a912c85b307762e40ea88add998ff0ef5
Abhishek Saklani <saklani1408@gmail.com>
2020-09-30 17:01:28 +0530

    added basic auth

--- hungerDelight/app/models.py
+++ hungerDelight/app/models.py
@@ -35,6 +35,10 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=15, decimal_places=6)
     created_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True)
+    merchant = models.ForeignKey(
+        'Merchant',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.name
