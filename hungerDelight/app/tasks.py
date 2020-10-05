aeeeaee5d4680a3d77e43804e0b5a51554ab879f
Abhishek Saklani <saklani1408@gmail.com>
2020-10-05 17:20:48 +0530

    changed celery-tasks structure

--- hungerDelight/app/tasks.py
+++ hungerDelight/app/tasks.py
@@ -1,33 +1,35 @@
-from .celery import app
+from __future__ import absolute_import
+
+from celery import shared_task
 from rest_framework.test import APIClient
 from django.urls import reverse, resolve
 from django.contrib.auth.models import User
 
 
-@app.task
+@shared_task
 def generate_order(i):
     admin_user = User.objects.filter(
         username='abhi').first()
-    # print('admin user is', admin_user.password)
+
     client = APIClient()
     client.force_authenticate(user=admin_user)
     payment_mode = ['CARD', 'CASH', 'WALLET']
     merchant_id = 2 if i % 2 == 0 else 13
-    # print('merchant id is', merchant_id)
+
     store_map_merchant = {
         '2': [1, 3],
         '13': [6]
     }
     store = store_map_merchant.get(str(merchant_id))
     store_id = store[i % len(store)]
-    # print('store arr', store, ' store id', store_id)
+
     item_map_merchant = {
         '2': [2, 3],
         '13': [5]
     }
     item = item_map_merchant.get(str(merchant_id))
     item_id = item[i % len(item)]
-    # print('item arr', item, ' item id ', item_id)
+
     send_data_order = {
         "timestamp": "2020-10-01T22:10:00Z",
         "status": "SUCCESS",
@@ -39,13 +41,16 @@ def generate_order(i):
         ]
 
     }
-    # print('send order data', send_data_order)
     response_order = client.post(
         reverse('order-list'), data=send_data_order)
-    print('response is ', response_order)
-    print('RESPONSE: ', response_order.status_code)
+
+    return response_order.status_code
 
 
 def run(n):
+    async_ids = []
     for i in range(n):
-        generate_order.delay(i)
+        id_ = generate_order.delay(i)
+        async_ids.append(id_)
+
+    return async_ids
--- hungerDelight/project/celery.py
+++ hungerDelight/project/celery.py
@@ -10,7 +10,9 @@
 #     app.start()
 #     app.autodiscover_tasks()
 
+from __future__ import absolute_import
 import os
+
 
 from celery import Celery
 
@@ -19,7 +21,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 app = Celery('project', backend='rpc://',
              broker='amqp://guest@localhost//',
-             include=['project.tasks', 'project.generateorder'])
+             include=['app.tasks'])
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
