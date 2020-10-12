7c90490809b0e442627c1e92f5d7c744030cce6b
Abhishek Saklani <saklani1408@gmail.com>
2020-10-12 16:00:58 +0530

    added retry for task failing due to exception

--- hungerDelight/app/tasks.py
+++ hungerDelight/app/tasks.py
@@ -1,3 +1,4 @@
+from celery import states
 import json
 import requests
 from celery import shared_task
@@ -8,6 +9,7 @@ from .models import Merchant, Item, Store, Order
 from .serializers import MerchantSerializer, ItemSerializer, StoreSerializer, OrderSerializer
 from datetime import datetime
 import structlog
+from project.celery import app
 logger = structlog.get_logger()
 
 
@@ -72,8 +74,8 @@ def run(n):
     return async_ids
 
 
-@shared_task
-def create_order(validated_data):
+@app.task(bind=True, autoretry_for=(Exception,), max_retries=5, default_retry_delay=20)
+def create_order(self, validated_data):
     '''
         async order creation, it fetches from the queue for any order related data,
             if there is a data in queue, it creates the order for it in the models.
@@ -109,19 +111,25 @@ def create_order(validated_data):
     order.save()
     log.msg('success', task='order created successfully')
 
-    serial_order = OrderSerializer(order)
+    serial_order = OrderSerializer(data=order)
+    raise Exception('NOT PRESENT')
     return serial_order.data
 
 
-@shared_task
-def webhook(order_data):
+@app.task(bind=True)
+def webhook(self, order_data):
 
+    logger.msg('webook request sent', order_data=order_data)
     res = requests.post('https://339898b5fcbf.ngrok.io/',
                         json=order_data)
+
+    # if res.status_code == 400:
+    #     self.update_state(state=states.FAILURE)
+    # print(dir(self))
     return res
 
 
-@shared_task
-def myerror(uuid):
-    print('IN ERROR', uuid)
+@app.task(bind=True)
+def myerror(self, uuid):
+    logger.msg('webook request failed', uuid=uuid)
     return
